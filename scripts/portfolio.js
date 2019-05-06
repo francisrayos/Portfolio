c42c5d44382cbf468dc457689df24c73157b183d
Francis Rayos del Sol <francis.rayos@outlook.com>
2019-05-06 00:33:24 -0400

    added metaball cursor

---
+++ scripts/portfolio.js
@@ -0,0 +1,9 @@
+// SETUP CANVAS
+// var canvas = document.getElementById('myCanvas');
+// var context = canvas.getContext('2d');
+// context.canvas.width  = window.innerWidth;
+// context.canvas.height = window.innerHeight;
+// // context.arc(300, 200, 60, 0, Math.PI * 2, false);
+// // context.arc(100, 100, 60, 0, Math.PI * 2, false);
+// context.fillStyle = "#97e6ed";
+// context.fill()
