72199f88c7f5e6404c5499a2427e775c4de4f076
Francis Rayos del Sol <francis.rayos@outlook.com>
2019-05-25 00:58:41 -0400

    updated webpages and design

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,3 +1,5 @@
+// --------- ATTEMPT #1 ---------
+
 // SETUP CANVAS
 // var canvas = document.getElementById('myCanvas');
 // var context = canvas.getContext('2d');
@@ -7,3 +9,21 @@
 // // context.arc(100, 100, 60, 0, Math.PI * 2, false);
 // context.fillStyle = "#97e6ed";
 // context.fill()
+
+// --------- ATTEMPT #2 ---------
+
+// Fill Main Screen
+// var main;
+
+// main = function () { 
+//     container = d3.select("body");
+//     //container.html();
+//     container.append("div")
+//     .attr("id", "final-nutrition")
+//     .html("Some More Nutritional <br>Facts About Your Meal:" +
+//     "<br><br><p id=\"more\">Total Fat - <span id=\"fat\">0</span> g" +
+//     "<br><br>Sugars - <span id=\"sugar\">0</span> g" +
+//     "<br><br>Protein - <span id=\"protein\">0</span> g" +
+//     "<br><br>Carbohydrates - <span id=\"carbs\">0</span> g" +
+//     "<br><br>Cholesterol - <span id=\"cholesterol\">0</span> mg</p>");
+// }
